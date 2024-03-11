64c1b98e0e51fec6d0250f1c416ccceedb500ddf
santosisarocha <123705785+santosisarocha@users.noreply.github.com>
2024-03-11 16:45:31 -0300

    Add files via upload

--- atividade_fevereiro/aula4/function_html_css/exe2/script.js
+++ atividade_fevereiro/aula4/function_html_css/exe2/script.js
@@ -1,8 +1,8 @@
 function verificarPar() {
     var n1 = document.getElementById("numeroInput").value;
-    if (n1 % 2 === 0) {
-        alert("O número é par");
+    if (n1 % 2 === 0) {        
+        document.getElementById("resultado").innerText = "O número é par";
     } else {
-        alert("O número é ímpar");
+        document.getElementById("resultado").innerText = "O número é ímpar";
     }
 }
--- atividade_fevereiro/aula4/function_html_css/exe3/script.js
+++ atividade_fevereiro/aula4/function_html_css/exe3/script.js
@@ -4,17 +4,19 @@ function encontrarMaior() {
     var n3 = parseFloat(document.getElementById("numero3").value);
 
     if (isNaN(n1) || isNaN(n2) || isNaN(n3)) {
-        alert("Por favor, insira números válidos.");
+        document.getElementById("resultado").innerText = "Por favor, insira números válidos.";
         return;
     }
 
     if (n1 > n2 && n1 > n3) {
-        alert("O maior número é: " + n1);
+        document.getElementById("resultado").innerText = "O maior número é: " + n1;
+
     } else if (n2 > n1 && n2 > n3) {
-        alert("O maior número é: " + n2);
+        document.getElementById("resultado").innerText = "O maior número é: " + n2;
     } else if (n3 > n1 && n3 > n2) {
-        alert("O maior número é: " + n3);
+        document.getElementById("resultado").innerText = "O maior número é: " + n3;
     } else {
-        alert("Há números iguais ou algum erro ocorreu.");
+       
+        document.getElementById("resultado").innerText = "Há números iguais ou algum erro ocorreu.";
     }
 }
